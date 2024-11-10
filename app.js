e342260af7ceb0e57cacad03130761235b0b0f4d
Yunis003 <hi.yunisqulamov@gmail.com>
2024-11-10 19:15:55 +0400

    design

--- app.js
+++ app.js
@@ -1,70 +1,65 @@
-let input = document.querySelector(".add")
+let input = document.querySelector(".add");
 let btn = document.querySelector(".add-btn");
 let lists = document.querySelector(".lists-container");
 
+function addTask() {
+  //failed
+  if (input.value == "") {
+    input.style.backgroundColor = "crimson";
+  }
+  //main
+  else {
+    let li = document.createElement("li");
+    li.textContent = input.value;
+    li.style.marginBottom = "10px";
+    lists.appendChild(li);
+    let img = document.createElement("img");
+    img.src = "./assets/img/non_hover.svg";
+    li.append(img);
 
-
-function addTask(){
-    //input bosdursa
-    if(input.value == ''){
-        input.style.backgroundColor = "crimson";
-    }
-    //esas hisse
-    else{
-        let li = document.createElement('li');
-        li.textContent = input.value;
-        li.style.marginBottom = "10px";
-        lists.appendChild(li);
-        let img=document.createElement('img')
-        img.src= "./assets/img/non_hover.svg"
-        li.append(img);
-
-        img.addEventListener('mouseenter', ()=>{
-            img.src = "./assets/img/hover.svg"
-        })
-        img.addEventListener('mouseleave', ()=>{
-            img.src = "./assets/img/non_hover.svg"
-        })
-        
-    }
+    img.addEventListener("mouseenter", () => {
+      img.src = "./assets/img/hover.svg";
+    });
+    img.addEventListener("mouseleave", () => {
+      img.src = "./assets/img/non_hover.svg";
+    });
+  }
 }
 
-lists.addEventListener("click", (e)=>{
-    if (e.target.tagName === 'IMG'){
-        e.target.parentElement.remove();
-    }
-})
-
+lists.addEventListener("click", (e) => {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+  }
+});
 
 let filter = document.querySelector(".filter");
 let filter2 = document.querySelector(".filter2");
-filter2.style.display = 'none';
+filter2.style.display = "none";
 
-filter.addEventListener("click", ()=>{
-    let items = Array.from(lists.children);
+//? A-Z Filter
+filter.addEventListener("click", () => {
+  let items = Array.from(lists.children);
 
-    for (let i = 0; i < items.length; i++){
-        if(items[i].textContent.localeCompare(items[i+1].textContent) > 0){
-            items.sort((a, b) => a.textContent.localeCompare(b.textContent));
-         items.forEach(item => lists.appendChild(item));
-         filter.style.display = 'none';
-         filter2.style.display = 'block';
-         
-
-        }
-        
-        //! bele bir sey eleki basanda img deyissin o birine bu biri sekil itsin ama sora o sekile klik eliyende tersine olaraq o sekilde filtrizasiya elesin.
-        
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].textContent.localeCompare(items[i + 1].textContent) > 0) {
+      items.sort((a, b) => a.textContent.localeCompare(b.textContent));
+      items.forEach((item) => lists.appendChild(item));
+      filter.style.display = "none";
+      filter2.style.display = "block";
     }
-})
+    //! bele bir sey eleki basanda img deyissin o birine bu biri sekil itsin ama sora o sekile klik eliyende tersine olaraq o sekilde filtrizasiya elesin.
+  }
+});
 
-filter2.addEventListener("click", ()=>{
-    let items = Array.from(lists.children);
-    for (let i = 0; i < items.length; i++){
-        if(items[i].textContent.localeCompare(items[i+1].textContent) < 0){
-            items.sort((a, b) => b.textContent.localeCompare(a.textContent));
-         items.forEach(item => lists.appendChild(item));
-         filter2.style.display = 'none';
-         filter.style.display = 'block';
+//? Z-A Filter
+filter2.addEventListener("click", () => {
+  let items = Array.from(lists.children);
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].textContent.localeCompare(items[i + 1].textContent) < 0) {
+      items.sort((a, b) => b.textContent.localeCompare(a.textContent));
+      items.forEach((item) => lists.appendChild(item));
+      filter2.style.display = "none";
+      filter.style.display = "block";
     }
-    }})
+  }
+});
