ebb49e3abaa27499b5c47931f57361f49bfca93e
Yunis003 <hi.yunisqulamov@gmail.com>
2024-11-10 09:20:40 +0400

    finalize_functioanlity

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let input = document.querySelector(".add")
 let btn = document.querySelector(".add-btn");
 let lists = document.querySelector(".lists-container");
 
+
+
 function addTask(){
     //input bosdursa
     if(input.value == ''){
@@ -11,15 +13,24 @@ function addTask(){
     else{
         let li = document.createElement('li');
         li.textContent = input.value;
+        li.style.marginBottom = "10px";
         lists.appendChild(li);
-        let span=document.createElement('span')
-        span.innerHTML='&#x2717'
-        li.append(span);
+        let img=document.createElement('img')
+        img.src= "./assets/img/non_hover.svg"
+        li.append(img);
+
+        img.addEventListener('mouseenter', ()=>{
+            img.src = "./assets/img/hover.svg"
+        })
+        img.addEventListener('mouseleave', ()=>{
+            img.src = "./assets/img/non_hover.svg"
+        })
+        
     }
 }
 
 lists.addEventListener("click", (e)=>{
-    if (e.target.tagName === 'SPAN'){
+    if (e.target.tagName === 'IMG'){
         e.target.parentElement.remove();
     }
 })
